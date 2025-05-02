8425c07278660d10b039b4ff733685a7cfc3b514
nindgabeet <wenming.soh@gmail.com>
2025-05-03 00:12:50 +0800

    finished project

--- main/main.go
+++ main/main.go
@@ -15,12 +15,22 @@ func main() {
 	if err != nil {
 		log.Fatal("error occured when initialising server: ", err)
 	}
-	introHTML, err := htmlGenerator.GenerateHTML(storyChoices["intro"])
 	if err != nil {
 		log.Fatal("error occured when initialising server: ", err)
 	}
+	storyHTMLS, err := htmlGenerator.GetHTMLS(storyChoices)
+	if err != nil {
+		log.Fatal("error occured when initialising server: ", err)
+	}
+
 	mux := server.InitServer()
-	mux.Handle("/", server.DefaultHandler(introHTML))
+	mux.Handle("/", server.DefaultHandler(storyHTMLS["intro"]))
+	mux.Handle("/new-york", server.DefaultHandler(storyHTMLS["new-york"]))
+	mux.Handle("/debate", server.DefaultHandler(storyHTMLS["debate"]))
+	mux.Handle("/sean-kelly", server.DefaultHandler(storyHTMLS["sean-kelly"]))
+	mux.Handle("/mark-bates", server.DefaultHandler(storyHTMLS["mark-bates"]))
+	mux.Handle("/denver", server.DefaultHandler(storyHTMLS["denver"]))
+	mux.Handle("/home", server.DefaultHandler(storyHTMLS["home"]))
 	fmt.Println("listening on port 8080")
 	http.ListenAndServe(":8080", mux)
 }
